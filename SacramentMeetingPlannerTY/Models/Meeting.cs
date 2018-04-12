f500c8f9d6bd4cdde2559c6626044932d76787d3
Trevor Albrethsen <Trevor Albrethsen@DESKTOP-3Q9T85O>
2018-04-12 11:29:20 -0600

    Minor changes

--- SacramentMeetingPlannerTY/Models/Meeting.cs
+++ SacramentMeetingPlannerTY/Models/Meeting.cs
@@ -12,46 +12,57 @@ namespace SacramentMeetingPlannerTY.Models
         [DataType(DataType.Date)]
         public DateTime MeetingDate { get; set; }
 
+        [Display(Name = "Conducting")]
         [StringLength(60, MinimumLength = 3)]
         [Required]
         public string Bishopric { get; set; }
 
+        [Display(Name = "Opening Hymn")]
         [StringLength(60, MinimumLength = 3)]
         [Required]
         public string OpeningHymn { get; set; }
 
+        [Display(Name = "Opening Prayer")]
         [StringLength(60, MinimumLength = 3)]
         [Required]
         public string OpeningPray { get; set; }
 
+        [Display(Name = "Sacrament Hymn")]
         [StringLength(60, MinimumLength = 3)]
         [Required]
         public string SacramentHymn { get; set; }
 
+        [Display(Name = "First Speaker")]
         [StringLength(60, MinimumLength = 3)]
         [Required]
         public string FirstSpeaker { get; set; }
 
+        [Display(Name = "Second Speaker")]
         [StringLength(60, MinimumLength = 3)]
         [Required]
         public string SecondSpeaker { get; set; }
 
+        [Display(Name = "Intermediate Hymn")]
         [StringLength(60, MinimumLength = 3)]
         [Required]
         public string IntermediateHymn { get; set; }
 
+        [Display(Name = "Third Speaker")]
         [StringLength(60, MinimumLength = 3)]
         [Required]
         public string ThirdSpeaker { get; set; }
 
+        [Display(Name = "Fourth Speaker")]
         [StringLength(60, MinimumLength = 3)]
         [Required]
         public string FourthSpeaker { get; set; }
 
+        [Display(Name = "Closing Hymn")]
         [StringLength(60, MinimumLength = 3)]
         [Required]
         public string ClosingHymn { get; set; }
 
+        [Display(Name = "Closing Prayer")]
         [StringLength(60, MinimumLength = 3)]
         [Required]
         public string ClosingPrayer { get; set; }
